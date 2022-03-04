ef2f0a33e87df8642ff1f491271ab5361a5232ca
ceciPino <mepino@uc.cl>
2022-03-04 12:11:14 -0300

    avance en el enrutamiento y el sueño de la casa propia

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@
 
 //import { myFunction } from './lib/index.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+//import { app } from "../firebase-config.js";
 import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 import { headerContainer } from "./pages/header.js";
 import { login } from "./pages/logIn.js";
-//import { route } from "./route.js";
+import { route } from "./route.js";
 import { register } from "./pages/register.js";
 //import { firestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
-
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -25,34 +21,20 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth();
 
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
+const auth = getAuth();
 
 
 onAuthStateChanged(auth, (user) => {
   if (user !== null) {
       console.log("logged in");
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    //const uid = user.uid;
-    // ...
+ 
   } else {
       console.log("no user");
     // User is signed out
-    // ...
   }
 });
 
@@ -61,27 +43,8 @@ onAuthStateChanged(auth, (user) => {
 let rootHeader = document.getElementById("rootHeader");
 rootHeader.appendChild(headerContainer());
 
-let root = document.getElementById("root");
 
+route ();
 
-window.addEventListener('hashchange', () => {//hashchange es lo que ocurre cuando se agrega el # en la url
-  if (window.location.hash === '#login'){
-    console.log('mostrar about')
-    root.appendChild(login());// Paso 1.2
-    changeRoute(window.location.hash);
-  } else if (window.location.hash === '#register'){
-    console.log('mostrar about')
-    login().remove();
-    root.appendChild(register()); // Paso 1.2
-    changeRoute(window.location.hash);
-  } 
-});
-
-
-// let root = document.getElementById("root");
-// root.appendChild(register());
-
-//route ();
-//register();
 
 
