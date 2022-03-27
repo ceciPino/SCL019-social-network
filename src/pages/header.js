7e72e097050ee83781e15ceee20ade61aa14be79
majesuso <majesus.op@gmail.com>
2022-03-27 17:37:01 -0300

    prueba ruta img

--- src/pages/header.js
+++ src/pages/header.js
@@ -8,7 +8,7 @@ export const headerContainer = () => {
     divHeader.appendChild(createHeader);
 
     let logo = document.createElement("img");
-    logo.setAttribute("src", "./src/images/logo-plantasia.svg");
+    logo.setAttribute("src", "../images/logo-plantasia.svg");
     logo.setAttribute("class", "logo");
     createHeader.appendChild(logo);
 
